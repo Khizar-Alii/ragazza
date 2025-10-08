3d9a029ac41a10a9370d316684002ff9b231d7f2
khizar-axonyxverse <khizar.ali@axonyxverse.com>
2025-10-08 15:15:56 +0500

    updated code

--- src/components/twoColumnSection/twoColumnSection.tsx
+++ src/components/twoColumnSection/twoColumnSection.tsx
@@ -1,15 +1,9 @@
 import styles from "./twoColumnSection.module.css";
-import SilviaImage from "../../assets/silvia2.png";
+
 const TwoColumnSection = () => {
   return (
     <div className={`${styles.twoColContainer} pageContainer`}>
       <div className={styles.contentWrapper}>
-        <div className={styles.imageColumn}>
-          <div className={styles.imageFrame}>
-            <img src={SilviaImage} alt="Silvia" className={styles.image} />
-          </div>
-        </div>
-
         <div className={styles.textColumn}>
           <h2 className={styles.title}>
             La storia di una madre e di una donna provata dal destino, il
@@ -26,14 +20,14 @@ const TwoColumnSection = () => {
               Uno stile linguistico che cambia, si modula ad ogni passaggio: da
               una prosa sciolta, che corre lungo il binario dei pensieri,
               facendosi lirica e allo stesso tempo aspramente realistica, alla
-              scarnificazione del monologo interiore al l imite del libero
+              scarnificazione del monologo interiore al limite del libero
               flusso di coscienza, fino al dialogo spontaneo che rende
               sopportabile una conversazione drammatica e impossibile.
               <br />
               I sei quadri contengono la sofferenza straziante di una donna e di
               una madre a cui la vita ha riservato la prova più dolorosa: la
               perdita di una figlia. Questo il tema che il libro affronta con
-              sorprendente coraggio e vivido realismo, i mmergendosi senza
+              sorprendente coraggio e vivido realismo, immergendosi senza
               anestetici nel vortice di domande e di disperazione della
               protagonista, privata del suo mondo affettivo e della percezione
               di sé.
@@ -53,7 +47,6 @@ const TwoColumnSection = () => {
               disperazione della protagonista, privata del suo mondo affettivo e
               della percezione di sé.
             </p>
-            <p className={styles.signature}>Luisa Fontana</p>
           </div>
         </div>
       </div>
